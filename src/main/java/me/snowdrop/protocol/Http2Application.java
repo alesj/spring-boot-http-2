f7ccd167e6d9a1166fce3942ea10ef4494ad8916
Ales Justin <ales.justin@gmail.com>
2018-01-25 14:03:28 +0100

    Use Undertow for HTTP2.

--- src/main/java/me/snowdrop/protocol/Http2Application.java
+++ src/main/java/me/snowdrop/protocol/Http2Application.java
@@ -1,13 +1,10 @@
 package me.snowdrop.protocol;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.catalina.core.AprLifecycleListener;
-import org.apache.coyote.http2.Http2Protocol;
+import io.undertow.UndertowOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,40 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Http2Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Http2Application.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Http2Application.class, args);
+    }
 
-	@Bean
-	public EmbeddedServletContainerCustomizer tomcatCustomizer() {
-		return (container) -> {
-			if (container instanceof TomcatEmbeddedServletContainerFactory) {
-				((TomcatEmbeddedServletContainerFactory) container)
-						.addConnectorCustomizers((connector) -> {
-							connector.addUpgradeProtocol(new Http2Protocol());
-							connector.addLifecycleListener(new AprLifecycleListener());
-						});
-			}
-		};
-	}
-
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory container = new TomcatEmbeddedServletContainerFactory();
-		container.addAdditionalTomcatConnectors(createStandardConnector());
-		return container;
-	}
-
-	private Connector createStandardConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.addUpgradeProtocol(new Http2Protocol());
-		connector.setPort(8080);
-		return connector;
-	}
+    @Bean
+    public EmbeddedServletContainerCustomizer undertowCustomizer() {
+        return (container) -> {
+            if (container instanceof UndertowEmbeddedServletContainerFactory) {
+                UndertowEmbeddedServletContainerFactory factory = (UndertowEmbeddedServletContainerFactory) container;
+                factory.addBuilderCustomizers(
+                    builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true)
+                );
+            }
+        };
+    }
 
 
-	@RequestMapping
-	String hello() {
-		return "hello!";
-	}
+    @RequestMapping("/")
+    String hello() {
+        return "hello!";
+    }
 }
